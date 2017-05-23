9a1f1b47d7f71bfb7001d41ce3a2549b900a5ff4
Daniel Z. <dzallinger@gmx.at>
2017-05-23 20:52:57 +0200

    Updated code

--- Starbound PackManager/Starbound PackManager/MainWindow.xaml.cs
+++ Starbound PackManager/Starbound PackManager/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -23,6 +25,8 @@ namespace Starbound_PackManager
         {
             InitializeComponent();
 
+            Rectangle.Background = new SolidColorBrush(Color.FromRgb(226, 226, 226));
+
         }
 
         private void Rectangle_Drop(object sender, DragEventArgs e)
@@ -36,17 +40,62 @@ namespace Starbound_PackManager
 
                 // Assuming you have one file that you care about, pass it off to whatever
                 // handling code you have defined.
+                foreach (var path in files)
+                {
+                    FileInfo fi = new FileInfo(path);
 
+                }
+                
                 string source = files[0];
                 using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
                 {
                     System.Windows.Forms.DialogResult result = dialog.ShowDialog();
                     dialog.SelectedPath = g.App_Path;
-                    g.FileDestinaton = dialog.SelectedPath;
-                }
-                
 
+                    InputPath.Text = files[0];
+
+                    g.FileDestinaton = dialog.SelectedPath;
+                    OutputPath.Text = dialog.SelectedPath;
+                }
             }
+        }
+
+        private void Unpack_Click(object sender, RoutedEventArgs e)
+        {
+            Global g = new Global();
+           
+            Process process = new Process();
+            process.StartInfo.FileName = "notepad.exe";
+            process.StartInfo.WorkingDirectory = @"c:\temp";
+            process.StartInfo.Arguments = @"C:\Users\Zassi\Desktop\Neuer Ordner (2)";
+            process.EnableRaisingEvents = true;
+            process.Start();
+            if (process.HasExited)
+            {
+                //do something
+            }
+
+
+        }
+
+        private void Rectangle_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+                var listbox = sender as ListBox;
+                listbox.Background = new SolidColorBrush(Color.FromRgb(155, 155, 155));
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void Rectangle_DragLeave(object sender, DragEventArgs e)
+        {
+            var listbox = sender as ListBox;
+            listbox.Background = new SolidColorBrush(Color.FromRgb(226, 226, 226));
         }
     }
 }
